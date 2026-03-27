3ff563f49145f2b1db6c124e44bd227dd2fb1928
christovjohn <christovarghesejohn@gmail.com>
2026-03-27 12:57:31 +0530

    Update hover effects and index.js modifications

--- thomson_project_1/wwwroot/js/index.js
+++ thomson_project_1/wwwroot/js/index.js
@@ -1,5 +1,11 @@
-﻿document.querySelectorAll('.flip-card').forEach(card => {
+document.querySelectorAll('.flip-card').forEach(card => {
     card.addEventListener('click', () => {
         card.classList.toggle('flipped');
+    });
+});
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('hovered');
     });
 });
